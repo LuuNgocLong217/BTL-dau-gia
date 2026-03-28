a5e6b82154878a0f5fc4296159724fd0ae165ec2
Long <lluungoc22@gmail.com>
2026-03-28 11:58:18 +0700

    Long beo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
-import org.w3c.dom.ls.LSOutput;
 
-import javax.management.MBeanOperationInfo;
-
-LSOutput khoa MBeanOperationInfo
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
